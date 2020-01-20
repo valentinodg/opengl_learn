6739d07e44f5c859cd23d6d720a6ee2bf4af65db
valentinodg <valentino.digiosaffatte@student.univaq.it>
2020-01-20 16:41:29 +0100

    .

---
+++ 3-5 shaders ex-2/main.cpp
@@ -0,0 +1,121 @@
+//pre-processing
+#include "../lib/glad/glad.h"
+#include </usr/include/GLFW/glfw3.h>
+
+#include "shader.hpp"
+
+#include <iostream>
+#include <cmath>
+
+//namespaces
+using namespace std;
+
+//functions prototypes
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+//macros
+const unsigned int SCREEN_WIDTH = 800;
+const unsigned int SCREEN_HEIGHT = 600;
+
+//main
+int main()
+{
+
+    //glfw: initialization
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    //glfw: window creation
+    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "gl-window", NULL, NULL);
+    if(window == NULL)
+    {
+        cout << "failed to create glfw window" << endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    //glad: load all opengl function pointers 
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        cout << "failed to initialize GLAD" << endl;
+        return -1;
+    }
+
+    //build and compile the shader program
+    Shader ourShader("3.3.shader.vertex", "3.3.shader.fragment");
+    //let's declare the x offset
+    float offset = 0.5f;
+
+
+    //vertex data & buffer(s) set up and vertex attributes configuration
+    float vertices[] = {
+    //positions (x,y,z)     //colors (rgb)
+     0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f,     //right
+    -0.5f, -0.5f, 0.0f,     0.0f, 1.0f, 0.0f,     //left
+     0.0f,  0.5f, 0.0f,     0.0f, 0.5f, 1.0f      //top
+    };
+
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    //position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+    //color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    //render loop
+    while(!glfwWindowShouldClose(window))
+    {   
+        //input
+        processInput(window);
+
+        //render
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //triangle draw
+        ourShader.use();
+
+        //let's set the x offset
+        ourShader.setFloat("xOffset", offset);
+
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        
+        //buffers swap and io events poll
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    
+    glfwTerminate();
+    return 0;
+}
+
+//functions definition
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
