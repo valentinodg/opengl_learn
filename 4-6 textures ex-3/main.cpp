d2c0d34a84a766a7da17ec4404163d273c930b23
valentinodg <valentino.digiosaffatte@student.univaq.it>
2020-01-21 17:24:14 +0100

    .

---
+++ 4-6 textures ex-3/main.cpp
@@ -0,0 +1,197 @@
+//pre-processing
+#include "../lib/glad/glad.h"
+#include </usr/include/GLFW/glfw3.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "../lib/stb_image.h"
+
+#include "shader.hpp"
+
+#include <iostream>
+#include <cmath>
+
+//namespaces
+using namespace std;
+
+//functions prototypes
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+//macros
+const unsigned int SCREEN_WIDTH = 800;
+const unsigned int SCREEN_HEIGHT = 600;
+
+//main
+int main()
+{
+
+    //glfw: initialization
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    //glfw: window creation
+    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "gl-window", NULL, NULL);
+    if(window == NULL)
+    {
+        cout << "failed to create glfw window" << endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    //glad: load all opengl function pointers 
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        cout << "failed to initialize GLAD" << endl;
+        return -1;
+    }
+
+    //build and compile the shader program
+    Shader ourShader("4.1.texture.vertex", "4.1.texture.fragment");
+
+    //vertex data & buffer(s) set up and vertex attributes configuration
+    float vertices[] = {
+    //positions (x,y,z)     //colors (rgb)        //texture coords  
+     0.5f,  0.5f, 0.0f,     1.0f, 0.0f, 0.0f,     0.55f, 0.55f, //top right
+     0.5f, -0.5f, 0.0f,     0.0f, 1.0f, 0.0f,     0.55f, 0.45f, //bottom right
+    -0.5f, -0.5f, 0.0f,     0.0f, 0.0f, 1.0f,     0.45f, 0.45f, //bottom left
+    -0.5f,  0.5f, 0.0f,     1.0f, 1.0f, 0.0f,     0.45f, 0.55f  //top left
+    };
+    unsigned int indices[] = {
+        0, 1, 3, //first triangle
+        1, 2, 3  //second triangle
+    };
+
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+  
+    //position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+    //color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    //texture coord attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    //load and create a texture
+    unsigned int texture1;
+    unsigned int texture2;
+
+    //texture1
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    //set texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    //set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    //load image, create texture and generate mipmaps
+    int width, height, nrChannels;
+    //vertical flip (y-axis)
+    stbi_set_flip_vertically_on_load(true);
+    //texture image path
+    unsigned char *data = stbi_load("./textures/container.jpg", &width, &height, &nrChannels, 0);
+    if(data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+    //texture2
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    //set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    //set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    //load image, create texture and generate mipmaps
+    data = stbi_load("./textures/awesomeface.png", &width, &height, &nrChannels, 0);
+    if(data)
+    {
+        //png -> alpha channel -> rgba
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+    //set texture units
+    //let's activate the shader
+    ourShader.use();
+    //manual set
+    glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
+    //set via the texture class
+    ourShader.setInt("texture2", 1);
+
+    //render loop
+    while(!glfwWindowShouldClose(window))
+    {   
+        //input
+        processInput(window);
+
+        //render
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //bind texture
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+
+        //render container
+        ourShader.use();
+
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        
+        //buffers swap and io events poll
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    
+    glfwTerminate();
+    return 0;
+}
+
+//functions definition
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
